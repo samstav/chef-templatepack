df25e65085d1f6f3783fe9cc94c337fae0f7cbdb
Seandon Mooy <admin@erulabs.com>
2015-02-04 17:26:47 -0600

    A Stack of PHP things. PHP56, MariaDB, Redis, Nginx, Wordpress and more. Includes editorconfig and modifies the base/recipes/default.rb for rackspace support purposes.

--- base/metadata.rb
+++ base/metadata.rb
@@ -5,3 +5,8 @@ license          'All rights reserved'
 description      'Installs/Configures |{ .Name }|'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'platformstack'
+depends 'users'
+depends 'locale'
+depends 'rackops_rolebook'
---
+++ stencils/nodejs/recipes/_nodejs.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: |{ .Cookbook.Name }|
+# Recipe :: |{ .Options.Name }|
+#
+# Copyright |{ .Cookbook.Year }|, Rackspace
+#
+
+include_recipe 'nodejs'
+
+# Install an NPM globally:
+# nodejs_npm 'cloudsink'
