b3cce48937f666f6153c9ac5fbc31b65b09ee1e7
erulabs <seandon.mooy@gmail.com>
2015-03-26 16:22:35 -0500

    improve base kitchen file with env vars

--- base/test/unit/spec/default_spec.rb
+++ base/test/unit/spec/default_spec.rb
@@ -2,6 +2,6 @@ require_relative 'spec_helper'
 
 describe '|{ cookbook['name'] }|::default' do
   let(:chef_run) do
-  ChefSpec::Runner.new.converge('|{ cookbook['name'] }|::default')
+    ChefSpec::Runner.new.converge('|{ cookbook['name'] }|::default')
   end
 end
